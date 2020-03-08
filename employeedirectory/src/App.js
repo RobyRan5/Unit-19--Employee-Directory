caa69a37e26b0b0238408120e754dd5855908c43
Roby Ran <robyran@icloud.com>
2020-03-07 23:58:01 -0600

    final push for unit 19 homework

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
 function App() {
   return (
--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,1 +1,17 @@
-    
+import useAxios from 'axios-hooks'
+ 
+function App() {
+  const [{ data, loading, error }, refetch] = useAxios(
+    'https://api.myjson.com/bins/820fc'
+  )
+ 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+ 
+  return (
+    <div>
+      <button onClick={refetch}>refetch</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
